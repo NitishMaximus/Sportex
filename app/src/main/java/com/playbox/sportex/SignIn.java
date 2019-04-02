17e456577c4f878ae9997e41eff3a136cfa76d3c
NitishMaximus <nitish07maximus@gmail.com>
2019-04-02 20:35:32 +0530

    2nd Initial Commit

---
+++ app/src/main/java/com/playbox/sportex/SignIn.java
@@ -0,0 +1,70 @@
+package com.playbox.sportex;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.playbox.sportex.Model.User;
+
+public class SignIn extends AppCompatActivity {
+
+    EditText edtPhone, edtPassword;
+    Button login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+        edtPhone = (EditText)findViewById(R.id.edtPhone);
+        edtPassword = (EditText)findViewById(R.id.edtPassword);
+        login = (Button)findViewById(R.id.button);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference table_user = database.getReference("User");
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                table_user.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        //get User Information
+                            if(dataSnapshot.child(edtPhone.getText().toString()).exists()) {
+                                User user = dataSnapshot.child(edtPhone.getText().toString()).getValue(User.class);
+                                if (user.getPassword().equals(edtPassword.getText().toString())) {
+                                    Toast.makeText(SignIn.this, "Sign In Success", Toast.LENGTH_SHORT).show();
+                                    Intent homeintent = new Intent(SignIn.this, HomePage.class);
+                                    startActivity(homeintent);
+
+                                } else {
+                                    Toast.makeText(SignIn.this, "Sign In Failed", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                            else
+                            {
+                                Toast.makeText(SignIn.this, "User does not exists", Toast.LENGTH_SHORT).show();
+                            }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        });
+    }
+}
